8003c95335a7f90ced3e031539bdc1dd905fd8a6
Thomas J <76921504+yellobyte@users.noreply.github.com>
2021-02-02 17:15:11 +0100

    First version released

--- src/SoapESP32.cpp
+++ src/SoapESP32.cpp
@@ -1,26 +1,26 @@
 /*
   SoapESP32, a simple library for accessing DLNA media servers with ESP32 devices
 	
-	Copyright (c) 2021 Thomas Jentzsch
+  Copyright (c) 2021 Thomas Jentzsch
 
-	Permission is hereby granted, free of charge, to any person
-	obtaining a copy of this software and associated documentation 
-	files (the "Software"), to deal in the Software without restriction, 
-	including without limitation the rights to use, copy, modify, merge, 
-	publish, distribute, sublicense, and/or sell copies of the Software, 
-	and to permit persons to whom the Software is furnished to do so, 
-	subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation 
+  files (the "Software"), to deal in the Software without restriction, 
+  including without limitation the rights to use, copy, modify, merge, 
+  publish, distribute, sublicense, and/or sell copies of the Software, 
+  and to permit persons to whom the Software is furnished to do so, 
+  subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be 
-	included in all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be 
+  included in all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-	 MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
-	ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
-	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
   Release Notes:
   V1.0.0  2021-02-02 Initial Version
@@ -64,20 +64,21 @@ xPathParser_t xmlParserPaths[] = {
 // find the first occurrence of what in s, ignore case.
 char *strcasestr(const char *s, const char *what)
 {
-	char c, sc;
-	size_t len;
-	if ((c = *what++) != 0) {
-		c = tolower((unsigned char)c);
-		len = strlen(what);
-		do {
-			do {
-				if ((sc = *s++) == 0)
-					return (NULL);
-			} while ((char)tolower((unsigned char)sc) != c);
-		} while (strncasecmp(s, what, len) != 0);
-		s--;
-	}
-	return ((char *)s);
+  char c, sc;
+  size_t len;
+  if ((c = *what++) != 0) {
+    c = tolower((unsigned char)c);
+    len = strlen(what);
+    do {
+	do {
+	  if ((sc = *s++) == 0) return (NULL);
+	} 
+      while ((char)tolower((unsigned char)sc) != c);
+    } 
+    while (strncasecmp(s, what, len) != 0);
+    s--;
+  }
+  return ((char *)s);
 }
 #endif
 
