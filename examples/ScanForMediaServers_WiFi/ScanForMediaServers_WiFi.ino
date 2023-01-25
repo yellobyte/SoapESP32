ff27936efce144a61365478f082f738330a9e1eb
Thomas Jentzsch <76921504+yellobyte@users.noreply.github.com>
2023-01-25 23:33:28 +0100

    Update ScanForMediaServers_WiFi.ino
    cosmetic changes

--- examples/ScanForMediaServers_WiFi/ScanForMediaServers_WiFi.ino
+++ examples/ScanForMediaServers_WiFi/ScanForMediaServers_WiFi.ino
@@ -4,15 +4,12 @@
   This sketch scans the local network for DLNA media servers
   using ESP32 builtin WiFi and prints them.
 
-  Last updated 2021-02-02, ThJ <yellobyte@bluewin.ch>
+  Last updated 2023-01-25, ThJ <yellobyte@bluewin.ch>
  */
 
 #include <Arduino.h>
 #include <WiFi.h>
 #include "SoapESP32.h"
-
-// uncomment in case you want to know
-//#define SHOW_ESP32_MEMORY_STATISTICS
 
 const char ssid[] = "MySSID";
 const char pass[] = "MyPassword"; 
@@ -30,7 +27,7 @@ void setup() {
   Serial.println();
   Serial.print("Connecting to WiFi network ");
   WiFi.begin(ssid, pass);
-  while ( WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
@@ -64,17 +61,6 @@ void setup() {
     Serial.print("  -> controlURL: ");
     Serial.println(srv.controlURL);
   }
-
-#ifdef SHOW_ESP32_MEMORY_STATISTICS
-  Serial.println("");
-  Serial.println("Some ESP32 memory stats after running this sketch:");
-  Serial.print(" 1) minimum ever free memory of all regions [in bytes]: ");
-  Serial.println(ESP.getMinFreeHeap());
-  Serial.print(" 2) minimum ever free heap size [in bytes]:             ");
-  Serial.println(xPortGetMinimumEverFreeHeapSize());
-  Serial.print(" 3) minimum ever stack size of this task [in bytes]:    ");
-  Serial.println(uxTaskGetStackHighWaterMark(NULL)); 
-#endif
 
   Serial.println();
   Serial.println("Sketch finished.");
